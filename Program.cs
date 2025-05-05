a55004a94404db21ee473b2e68a1a89f2416f484
goodmaker322 <93530396+goodmaker322@users.noreply.github.com>
2025-05-06 00:24:52 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using OnlineShop_G1.Middlewares;
+using OnlineShop_G1.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews().AddNewtonsoftJson();
+
+// Add session support
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(5);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+// Add DbContext
+builder.Services.AddDbContext<FStoreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+
+// Add authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Login/Index";
+        options.LogoutPath = "/Login/Index";
+    });
+
+var app = builder.Build();
+
+app.UseStatusCodePagesWithRedirects("/Checkout/Error?statuscode={0}");
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+// Add session and authentication middleware
+app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Add RoleMiddleware after authentication and session
+app.UseMiddleware<RoleMiddleware>();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
+    RequestPath = "/node_modules"
+});
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
